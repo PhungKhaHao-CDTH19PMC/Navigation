0e34da8e317ae8076b8e09aeba50d4d5b9445156
phungkhahao <phungkhahao@gmail.com>
2021-11-02 15:40:22 +0700

    add form sign in and form error

---
+++ lib/main.dart
@@ -0,0 +1,266 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
+}
+class Todo {
+  final int a;
+  final String title;
+  final String description;
+  const Todo(this.a,this.title, this.description);
+}
+
+TextEditingController num1= new TextEditingController();
+TextEditingController num2= new TextEditingController();
+
+class FirstRoute extends StatefulWidget {
+  const FirstRoute({Key? key}) : super(key: key);
+
+  @override
+  _FirstRouteState createState() => _FirstRouteState();
+}
+
+class _FirstRouteState extends State<FirstRoute> {
+  TextEditingController num1= new TextEditingController();
+  TextEditingController num2= new TextEditingController();
+  String result='';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[200],
+      body: Center(
+        child: 
+        Column(
+          children: [
+              Icon(
+                Icons.email_sharp,
+                color: Colors.pink,
+                size: 150,
+              ),
+              const Text(
+                'SignIn',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 70,
+                ),
+              ),
+              const Text(
+                'Speak, friend, and enter',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+              padding: const EdgeInsets.fromLTRB(32, 60, 32, 20),
+              child:  TextField(
+                controller: num1,
+                style: TextStyle(color: Colors.grey),
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Email',
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(32, 0, 32, 80),
+              child:  TextField(
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.grey),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Password',
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                controller: num2,
+              ),
+            ),
+
+            SizedBox(
+            width: 430.0,
+            height: 50.0,
+            child: OutlinedButton(
+              onPressed: (){
+                if(num1.text==''||num2.text=='')
+                {
+                  result="Vui lòng nhập đầy đủ thông tin";
+                }
+                else if(num1.text!=num2.text)
+                {
+                  Navigator.push(context,MaterialPageRoute(builder: (context) => const ThirtRoute()));
+                }
+                else
+                {
+                  Navigator.push(context,MaterialPageRoute(builder: (context) => const SecondRoute()));
+                }
+                setState(() {
+                  
+                });
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+              ),
+              child: const Text('SIGN IN', style: TextStyle(color: Colors.white),),
+            ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top:10),
+              child: Text(result),
+            )
+            
+          ],
+        ),
+
+      ),
+    );
+  }
+}
+  
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TodosScreen(
+        todos: List.generate(
+          
+          5,
+          (i) => Todo(
+            i,
+            'Todo $i',
+            'A description of what needs to be done for Todo $i',)
+            ),
+      )
+    );
+  }
+}
+
+class TodosScreen extends StatelessWidget {
+  const TodosScreen({Key? key, required this.todos}) : super(key: key);
+
+  final List<Todo> todos;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todos'),
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            
+            title: Text(todos[index].title),
+            // When a user taps the ListTile, navigate to the DetailScreen.
+            // Notice that you're not only creating a DetailScreen, you're
+            // also passing the current todo through to it.
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(todo: todos[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  // In the constructor, require a Todo.
+  const DetailScreen({Key? key, required this.todo}) : super(key: key);
+
+  // Declare a field that holds the Todo.
+  final Todo todo;
+
+  @override
+  Widget build(BuildContext context) {
+    // Use the Todo to create the UI.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Text(todo.a.toString()),
+            Text('   '),
+            Text(todo.description)
+          ],
+        ),
+      ),
+    );
+  }
+}
+class ThirtRoute extends StatelessWidget {
+  const ThirtRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: 
+        Column(
+          children: [
+              Icon(
+                Icons.email_sharp,
+                color: Colors.pink,
+                size: 150,
+              ),
+              const Text(
+                "UPS...countn' sign in",
+                style: TextStyle(
+                  fontWeight:FontWeight.bold,
+                ),
+              ),
+              const Text(
+                'Your username and password do not match',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+              const Text(
+                'Please try again',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(32, 100, 32, 32),
+                child: 
+                SizedBox(
+            width: 430.0,
+            height: 50.0,
+            child: OutlinedButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+              ),
+              child: const Text('TRY AGAIN', style: TextStyle(color: Colors.white),),
+            ),
+            ),
+                
+              )
+          ],
+        ),
+
+      ),
+    );
+  }
+}
